4f26525033e375957fb001ba80f44adbb64e0ca3
Nihed2001 <140057840+Nihed2001@users.noreply.github.com>
2023-11-06 15:00:09 +0100

    Update finder-test.sh

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Tester script for assignment 1 and assignment 2
-
+# Author: Siddhant Jajoo
 
 set -e
 set -u
@@ -15,7 +15,7 @@ then
 	echo "Using default value ${WRITESTR} for string to write"
 	if [ $# -lt 1 ]
 	then
-		echo "Using default value ${NUMFILES} for the number of files to write"
+		echo "Using default value ${NUMFILES} for number of files to write"
 	else
 		NUMFILES=$1
 	fi	
@@ -25,20 +25,21 @@ else
 	WRITEDIR=/tmp/aeld-data/$3
 fi
 
-MATCHSTR="The number of files is ${NUMFILES} and the number of matching lines is ${NUMFILES}"
+MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
 
-echo "Writing ${NUMFILES} files containing the string ${WRITESTR} to ${WRITEDIR}"
+echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat ../conf/assignment.txt`
+#WARNING! Changed as per A3-PART-2
+assignment=`cat conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
 	mkdir -p "$WRITEDIR"
 
-	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple arguments.
+	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
 	#The quotes signify that the entire string in WRITEDIR is a single string.
 	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
 	if [ -d "$WRITEDIR" ]
@@ -48,16 +49,20 @@ then
 		exit 1
 	fi
 fi
+#echo "Removing the old writer utility and compiling as a native application"
+#make clean
+#make
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	#./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
-rm -rf /tmp/aeld-data
+#rm -rf /tmp/aeld-data
 
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
