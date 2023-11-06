32a56e200182219c071a09477bf589a33689396e
Nihed2001 <140057840+Nihed2001@users.noreply.github.com>
2023-11-06 15:27:12 +0100

    Update systemcalls.c

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,136 +1,136 @@
-#include "systemcalls.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 
-/**
- * @param cmd the command to execute with system()
- * @return true if the command in @param cmd was executed
- *   successfully using the system() call, false if an error occurred,
- *   either in invocation of the system() call, or if a non-zero return
- *   value was returned by the command issued in @param cmd.
-*/
 bool do_system(const char *cmd)
 {
-
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
-    bool retVal = (system(cmd) == 0);
-
-    return retVal;
+    return system(cmd) == 0;
 }
-
-/**
-* @param count -The numbers of variables passed to the function. The variables are command to execute.
-*   followed by arguments to pass to the command
-*   Since exec() does not perform path expansion, the command to execute needs
-*   to be an absolute path.
-* @param ... - A list of 1 or more arguments after the @param count argument.
-*   The first is always the full path to the command to execute with execv()
-*   The remaining arguments are a list of arguments to pass to the command in execv()
-* @return true if the command @param ... with arguments @param arguments were executed successfully
-*   using the execv() call, false if an error occurred, either in invocation of the
-*   fork, waitpid, or execv() command, or if a non-zero return value was returned
-*   by the command issued in @param arguments with the specified arguments.
-*/
 
 bool do_exec(int count, ...)
 {
     va_list args;
     va_start(args, count);
-    char * command[count+1];
+    char *command[count + 1];
     int i;
-    for(i=0; i<count; i++)
+    for (i = 0; i < count; i++)
     {
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
-    
 
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
     int pid = fork();
     int retVal = 0;
-    
-    if(pid < 0) {
-    	printf("Error occurred in fork()\r\n");
-    	retVal = 1;
-    } else if(pid == 0) {
-    	//Child process code
-    	execv(command[0], command);
-    } else {
-    	//Parent process code, wait for all (there is only 1) child
-        waitpid(-1, &retVal, 0);
 
-	}
-    
+    if (pid < 0)
+    {
+        printf("Error occurred in fork()\n");
+        retVal = 1;
+    }
+    else if (pid == 0)
+    {
+        // Child process code
+        execv(command[0], command);
+        // If execv succeeds, this code won't be reached
+        perror("execv");
+        exit(1); // Exit with an error code
+    }
+    else
+    {
+        // Parent process code, wait for the child
+        waitpid(pid, &retVal, 0);
+    }
+
     va_end(args);
-    bool ret = (retVal == 0);
-    return ret;
+    return retVal == 0;
 }
 
-/**
-* @param outputfile - The full path to the file to write with command output.
-*   This file will be closed at completion of the function call.
-* All other parameters, see do_exec above
-*/
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
     va_list args;
     va_start(args, count);
-    char * command[count+1];
+    char *command[count + 1];
     int i;
-    for(i=0; i<count; i++)
+    for (i = 0; i < count; i++)
     {
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
 
-    va_end(args);
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
-    
     int pid = fork();
     int retVal = 0;
-    int fd = open(outputfile, O_WRONLY | O_CREAT);
-    
-    if(pid < 0) {
-    	printf("Error occurred in fork()\r\n");
-    	retVal = 1;
-    } else if(pid == 0) {
-    	//Child process code
-        if (dup2(fd, 1) < 0) { 
+    int fd = open(outputfile, O_WRONLY | O_CREAT, 0666);
+
+    if (pid < 0)
+    {
+        printf("Error occurred in fork()\n");
+        retVal = 1;
+    }
+    else if (pid == 0)
+    {
+        // Child process code
+        if (dup2(fd, 1) < 0)
+        {
             perror("dup2");
-            return 1;
-	}
-    	close(fd);
-    
-    	execv(command[0], command);
-    } else {
-    	//Parent process code, wait for all (there is only 1) child
-    	close(fd);
-    	wait(0);
-    	printf("DEBUG: parent return code %d\r\n", retVal);
+            exit(1); // Exit with an error code
+        }
+        close(fd);
+
+        execv(command[0], command);
+        // If execv succeeds, this code won't be reached
+        perror("execv");
+        exit(1); // Exit with an error code
+    }
+    else
+    {
+        // Parent process code, wait for the child
+        close(fd);
+        waitpid(pid, &retVal, 0);
     }
 
-    return (retVal == 0);
+    va_end(args);
+    return retVal == 0;
+}
+
+int main()
+{
+    // Example usage of do_system
+    const char *systemCommand = "ls -l";
+    if (do_system(systemCommand))
+    {
+        printf("do_system succeeded.\n");
+    }
+    else
+    {
+        printf("do_system failed.\n");
+    }
+
+    // Example usage of do_exec
+    const char *execCommand = "/bin/ls";
+    if (do_exec(2, execCommand, "-l"))
+    {
+        printf("do_exec succeeded.\n");
+    }
+    else
+    {
+        printf("do_exec failed.\n");
+    }
+
+    // Example usage of do_exec_redirect
+    const char *redirectCommand = "/bin/ls";
+    const char *outputFile = "output.txt";
+    if (do_exec_redirect(outputFile, 2, redirectCommand, "-l"))
+    {
+        printf("do_exec_redirect succeeded.\n");
+    }
+    else
+    {
+        printf("do_exec_redirect failed.\n");
+    }
+
+    return 0;
 }
